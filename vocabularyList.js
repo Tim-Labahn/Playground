798c7ce19506ad68191a8c66459aeb3befaa8e50
tim-labahn <tim.labahn@freaknet.de>
2023-08-07 10:16:03 +0200

    vocabel liste

--- vocabularyList.js
+++ vocabularyList.js
@@ -1,11 +1,12 @@
 const prompt = require("prompt-sync")({ sigint: true });
-
-const vocabularyList = [];
+//----------------------------------------------
+const vocabularyList = []
 
 function showList(arr) {
-    console.log("Inventory stores:", arr.length, "items");
+    console.log("inventory stors:", arr.length, "items"
+    )
     for (let i = 0; i < arr.length; i++) {
-        console.log(i + 1 + ".", arr[i].word, "-", arr[i].definition);
+        console.log(i + 1 + ".", arr[i].definition)
     }
 }
 
@@ -14,21 +15,28 @@ function addWord(word, definition) {
 }
 
 while (true) {
+    // vocabularyList.sort(function (a, b) {
+    //     return a.toLowerCase().localeCompare(b.toLowerCase());
+    // })
+
     console.log("Your options: A- add, D- delete, S- Show list, C- Change definition")
-    const event = prompt("What is your choice?");
+    const event = prompt("What is your choise?");
     if (event === "A") {
         let word = prompt("What do you wanna add?");
-        let definition = prompt("What is the definition?");
-        addWord(word, definition);
+        let definition = prompt("what is the definiton?")
+        addWord(word, definition)
+
     }
+    //  inventory.push('${ addItem }')
     if (event === "S") {
-        showList(vocabularyList);
+        showList(vocabularyList)
     }
     if (event === "D") {
         showList(vocabularyList)
         let deleteItem = prompt("Enter the name of what you want to delete?");
         vocabularyList.splice(vocabularyList.findIndex((a) => a === deleteItem), 1)
     }
+
     if (event === "C") {
         showList(vocabularyList)
         let word = prompt("Enter the name of the vocabular that you wana change.");
